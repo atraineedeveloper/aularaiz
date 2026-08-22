c91283d64b00ff1a1d65a1a05597a1f01a7c283c
Programisto <otilio.latam@gmail.com>
2026-08-22 14:49:30 -0600

    feat: expose persisted initial school setup

--- lib/application/contracts/school_setup_repository.dart
+++ lib/application/contracts/school_setup_repository.dart
@@ -1,8 +1,12 @@
 import 'package:aularaiz/domain/school/school.dart';
 import 'package:aularaiz/domain/school/school_year.dart';
 
+typedef InitialSchoolSetup = ({School school, SchoolYear schoolYear});
+
 abstract interface class SchoolSetupRepository {
   Future<bool> hasInitialSetup();
+
+  Future<InitialSchoolSetup?> loadInitialSetup();
 
   Future<void> saveInitialSetup({
     required School school,

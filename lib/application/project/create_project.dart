bef7bac4d4af1b04189709126d8edef5e1983531
Programisto <otilio.latam@gmail.com>
2026-08-22 16:08:49 -0600

    feat: add project creation use case

---
+++ lib/application/project/create_project.dart
@@ -0,0 +1,38 @@
+import 'package:aularaiz/application/contracts/project_repository.dart';
+import 'package:aularaiz/core/id/id_generator.dart';
+import 'package:aularaiz/domain/education/primary_grade.dart';
+import 'package:aularaiz/domain/project/formative_field.dart';
+import 'package:aularaiz/domain/project/project.dart';
+import 'package:aularaiz/domain/project/project_lifecycle.dart';
+import 'package:aularaiz/domain/project/project_methodology.dart';
+
+final class CreateProject {
+  CreateProject({
+    required ProjectRepository repository,
+    required IdGenerator idGenerator,
+  }) : _repository = repository,
+       _idGenerator = idGenerator;
+
+  final ProjectRepository _repository;
+  final IdGenerator _idGenerator;
+
+  Future<Project> call({
+    required String groupId,
+    required String title,
+    required ProjectMethodology methodology,
+    required FormativeField formativeField,
+    required Set<PrimaryGrade> targetGrades,
+  }) async {
+    final project = Project(
+      id: _idGenerator.newId(),
+      groupId: groupId,
+      title: title,
+      lifecycle: ProjectLifecycle.draft,
+      methodology: methodology,
+      formativeField: formativeField,
+      targetGrades: targetGrades,
+    );
+    await _repository.save(project);
+    return project;
+  }
+}

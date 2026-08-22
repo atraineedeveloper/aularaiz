ec33d03254abdca2fc6a68629fcd2e38c542ccff
Programisto <otilio.latam@gmail.com>
2026-08-22 17:31:05 -0600

    refactor: make AulaRaiz palette product-neutral

--- lib/app/theme/app_palette.dart
+++ lib/app/theme/app_palette.dart
@@ -1,39 +1,18 @@
 import 'package:flutter/material.dart';
 
-enum AppPalette { government2024, government2018 }
+/// AulaRaíz product colors.
+///
+/// The palette is intentionally product-owned and carries no institutional,
+/// political or administration label in the user experience.
+abstract final class AppPalette {
+  static const Color primary = Color(0xFF9B2247);
+  static const Color primaryDark = Color(0xFF611232);
+  static const Color secondary = Color(0xFF1E5B4F);
+  static const Color secondaryDark = Color(0xFF002F2A);
+  static const Color tertiary = Color(0xFFA57F2C);
+  static const Color tertiarySoft = Color(0xFFE6D194);
 
-extension AppPaletteColors on AppPalette {
-  Color get primary => switch (this) {
-    AppPalette.government2024 => const Color(0xFF9B2247),
-    AppPalette.government2018 => const Color(0xFF9D2449),
-  };
-
-  Color get primaryDark => switch (this) {
-    AppPalette.government2024 => const Color(0xFF611232),
-    AppPalette.government2018 => const Color(0xFF621132),
-  };
-
-  Color get secondary => switch (this) {
-    AppPalette.government2024 => const Color(0xFF1E5B4F),
-    AppPalette.government2018 => const Color(0xFF285C4D),
-  };
-
-  Color get secondaryDark => switch (this) {
-    AppPalette.government2024 => const Color(0xFF002F2A),
-    AppPalette.government2018 => const Color(0xFF13322B),
-  };
-
-  Color get tertiary => switch (this) {
-    AppPalette.government2024 => const Color(0xFFA57F2C),
-    AppPalette.government2018 => const Color(0xFFB38E5D),
-  };
-
-  Color get tertiarySoft => switch (this) {
-    AppPalette.government2024 => const Color(0xFFE6D194),
-    AppPalette.government2018 => const Color(0xFFD4C19C),
-  };
-
-  List<Color> get swatches => <Color>[
+  static const List<Color> swatches = <Color>[
     primary,
     primaryDark,
     secondary,

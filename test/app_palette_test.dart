eab496e8c6ef455b8563cad691a1942ff205401a
Programisto <otilio.latam@gmail.com>
2026-08-22 17:32:48 -0600

    test: lock the neutral AulaRaiz palette

--- test/app_palette_test.dart
+++ test/app_palette_test.dart
@@ -3,15 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('2024 palette keeps requested government-inspired colors', () {
-    expect(AppPalette.government2024.primary, const Color(0xFF9B2247));
-    expect(AppPalette.government2024.secondary, const Color(0xFF1E5B4F));
-    expect(AppPalette.government2024.tertiary, const Color(0xFFA57F2C));
-  });
-
-  test('2018 palette keeps requested government-inspired colors', () {
-    expect(AppPalette.government2018.primary, const Color(0xFF9D2449));
-    expect(AppPalette.government2018.secondary, const Color(0xFF285C4D));
-    expect(AppPalette.government2018.tertiary, const Color(0xFFB38E5D));
+  test('AulaRaiz palette keeps the requested product colors', () {
+    expect(AppPalette.primary, const Color(0xFF9B2247));
+    expect(AppPalette.secondary, const Color(0xFF1E5B4F));
+    expect(AppPalette.tertiary, const Color(0xFFA57F2C));
+    expect(AppPalette.swatches, hasLength(6));
   });
 }

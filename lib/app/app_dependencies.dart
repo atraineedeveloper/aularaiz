cb24efcd8e027341871f1a5481e407dbd6e49665
Programisto <otilio.latam@gmail.com>
2026-08-22 14:45:19 -0600

    feat: compose production school setup dependencies

---
+++ lib/app/app_dependencies.dart
@@ -0,0 +1,39 @@
+import 'package:aularaiz/application/contracts/school_setup_repository.dart';
+import 'package:aularaiz/application/school_setup/create_initial_school_setup.dart';
+import 'package:aularaiz/core/id/id_generator.dart';
+import 'package:aularaiz/core/id/uuid_id_generator.dart';
+import 'package:aularaiz/data/local/app_database.dart';
+import 'package:aularaiz/data/repositories/drift_school_setup_repository.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+class AppDependencies extends StatelessWidget {
+  const AppDependencies({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AppDatabase>(
+          create: (_) => AppDatabase.production(),
+          dispose: (_, database) => database.close(),
+        ),
+        Provider<IdGenerator>(create: (_) => UuidIdGenerator()),
+        Provider<SchoolSetupRepository>(
+          create: (context) => DriftSchoolSetupRepository(
+            context.read<AppDatabase>(),
+          ),
+        ),
+        Provider<CreateInitialSchoolSetup>(
+          create: (context) => CreateInitialSchoolSetup(
+            repository: context.read<SchoolSetupRepository>(),
+            idGenerator: context.read<IdGenerator>(),
+          ),
+        ),
+      ],
+      child: child,
+    );
+  }
+}

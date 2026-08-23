24f1fbc2c58f19738045a39db929bb1cec735cbf
Programisto <otilio.latam@gmail.com>
2026-08-23 16:21:14 -0600

    refactor: keep formative fields on activities only

--- lib/application/project/create_project.dart
+++ lib/application/project/create_project.dart
@@ -2,17 +2,14 @@ import 'package:aularaiz/application/contracts/project_repository.dart';
 import 'package:aularaiz/core/id/id_generator.dart';
 import 'package:aularaiz/domain/education/primary_grade.dart';
 import 'package:aularaiz/domain/project/articulating_axis.dart';
-import 'package:aularaiz/domain/project/formative_field.dart';
 import 'package:aularaiz/domain/project/project.dart';
 import 'package:aularaiz/domain/project/project_lifecycle.dart';
 import 'package:aularaiz/domain/project/project_methodology.dart';
 
 final class CreateProject {
-  CreateProject({
-    required ProjectRepository repository,
-    required IdGenerator idGenerator,
-  }) : _repository = repository,
-       _idGenerator = idGenerator;
+  CreateProject({required ProjectRepository repository, required IdGenerator idGenerator})
+      : _repository = repository,
+        _idGenerator = idGenerator;
 
   final ProjectRepository _repository;
   final IdGenerator _idGenerator;
@@ -21,7 +18,6 @@ final class CreateProject {
     required String groupId,
     required String title,
     required ProjectMethodology methodology,
-    required Set<FormativeField> formativeFields,
     Set<ArticulatingAxis> articulatingAxes = const <ArticulatingAxis>{},
     required Set<PrimaryGrade> targetGrades,
   }) async {
@@ -31,7 +27,6 @@ final class CreateProject {
       title: title,
       lifecycle: ProjectLifecycle.draft,
       methodology: methodology,
-      formativeFields: formativeFields,
       articulatingAxes: articulatingAxes,
       targetGrades: targetGrades,
     );

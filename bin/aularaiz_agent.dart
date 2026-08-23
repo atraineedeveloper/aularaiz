8f76b0e2c63dcab5629bb3aa708d9edd15e39999
Programisto <otilio.latam@gmail.com>
2026-08-23 08:58:21 -0600

    fix: satisfy analyzer in automation CLI

--- bin/aularaiz_agent.dart
+++ bin/aularaiz_agent.dart
@@ -122,7 +122,7 @@ Future<void> main(List<String> arguments) async {
     exitCode = 2;
   } on StateError catch (error) {
     _writeJson(
-      _errorEnvelope('data-state', '${error.message}'),
+      _errorEnvelope('data-state', error.message),
       pretty: pretty,
     );
     exitCode = 4;

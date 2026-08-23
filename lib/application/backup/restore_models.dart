2d372972bbf34d86d190c87130f6f0892c8bfd01
Programisto <otilio.latam@gmail.com>
2026-08-22 22:07:46 -0600

    feat: add typed restore workflow models

---
+++ lib/application/backup/restore_models.dart
@@ -0,0 +1,41 @@
+import 'package:aularaiz/application/backup/aularaiz_backup_codec.dart';
+
+enum RestoreProblem {
+  profileMismatch,
+  newerSchema,
+  invalidDatabase,
+  invalidRequest,
+  missingRestoreArtifact,
+  stagedArtifactChanged,
+  applyFailed,
+  rollbackFailed,
+}
+
+final class RestoreException implements Exception {
+  const RestoreException(this.problem, this.message, [this.cause]);
+
+  final RestoreProblem problem;
+  final String message;
+  final Object? cause;
+
+  @override
+  String toString() => 'RestoreException($problem): $message';
+}
+
+final class RestorePreview {
+  const RestorePreview({required this.manifest});
+
+  final BackupManifest manifest;
+}
+
+final class StagedRestore {
+  const StagedRestore({
+    required this.requestId,
+    required this.preview,
+  });
+
+  final String requestId;
+  final RestorePreview preview;
+}
+
+enum RestoreBootstrapOutcome { none, applied, rolledBack }

af4c319364b725b95cd55fc41bfd16dac24dccfa
Programisto <otilio.latam@gmail.com>
2026-08-23 19:50:46 -0600

    feat: add activity deletion capability

--- lib/application/contracts/activity_repository.dart
+++ lib/application/contracts/activity_repository.dart
@@ -7,3 +7,17 @@ abstract interface class ActivityRepository {
 
   Future<void> save(Activity activity);
 }
+
+abstract interface class DeletableActivityRepository {
+  Future<void> deleteActivity(String activityId);
+}
+
+extension ActivityRepositoryDeletion on ActivityRepository {
+  Future<void> deleteActivity(String activityId) {
+    final repository = this;
+    if (repository is DeletableActivityRepository) {
+      return repository.deleteActivity(activityId);
+    }
+    throw UnsupportedError('This activity repository does not support deletion.');
+  }
+}
